e6678a20902d9b033d73810489e0f4df78cb0249
raheal <raheal.mazumder@gmail.com>
2014-10-13 19:03:37 +0100

    Adding the web app project

---
+++ sampleapp-web/src/main/java/com/example/app/MyNewProject/SampleController.java
@@ -0,0 +1,14 @@
+package com.example.app.MyNewProject;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class SampleController {
+	@RequestMapping("/test")
+	public ModelAndView helloWorld(){
+		ModelAndView m = new ModelAndView("hello");
+		m.addObject("msg", "Hello world");
+		return m;
+	}
+}
